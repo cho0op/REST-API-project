5486a639f9898c3d2c48f806ba811652f7f48c0c
Artsiom <sorintema@gmail.com>
2020-09-05 10:58:04 +0300

    uploading images (only post method)

--- status/api/api_tests/rest_framework_api_tests.py
+++ status/api/api_tests/rest_framework_api_tests.py
@@ -1,8 +1,10 @@
 import json
-
+import os
 import requests
 
 ENDPOINT = "http://127.0.0.1:8000/api/status/"
+
+image_path = os.path.join(os.getcwd(), "bot-chto-takoe.jpg")
 
 
 def do_call(_id=13):
@@ -15,4 +17,15 @@ def do_call(_id=13):
     return r.text
 
 
-print(do_call())
+def do_call_with_image(image_path=None):
+    headers = {'Content-Type': "application/json"}
+    if image_path is not None:
+        with open(image_path, 'rb') as image:  # rb: reads as bytes
+            data = {"id":22, "user": 1, "content": "new content for imaged object"}
+            files_data = {"image": image}
+            r = requests.put(ENDPOINT, data=data, files=files_data, headers=headers)
+            print(r.text)
+            return r
+
+
+print(do_call_with_image(image_path))
